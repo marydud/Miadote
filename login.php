92d8de1e96b200f21a2b43ae9231c4eea5b24996
AnaJuliaTavares <109613769+AnaJuliaTavares@users.noreply.github.com>
2022-10-18 13:02:27 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login Miadote</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="form-image">
+            <div class="form">
+
+                <form action="#">
+                
+                <div class="logo">
+                    <img src="logogato.png" width="160" height="65" />
+                </div>
+
+                <div class="form-header">
+                    <div class="title">
+                        <h2>Entrar</h2>
+                    </div>
+                </div>
+
+                <div class="input-group">
+
+                    <div class="input-box">
+                        <label for="email">Email</label>
+                        <input id="email" type="email" name="email" placeholder="Digite seu email" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="password"">Senha</label>
+                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
+                    </div>
+
+                </div>
+                
+                <div class="continue-button">
+                    <button><a href="#">Continuar</a></button>
+                </div>
+
+                <div class="aa">
+                    <a href="cadastro.php">Criar nova conta</a>
+                </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
