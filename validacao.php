f05fea6f02e051d198da81cf7b5880a39332d8bb
AnaJuliaTavares <109613769+AnaJuliaTavares@users.noreply.github.com>
2022-10-20 14:54:02 -0300

    Update validacao.php

--- validacao.php
+++ validacao.php
@@ -6,14 +6,14 @@ if(isset($_POST['submit']))
 	include_once('conexaoo.php');
 
 		$nome = $_POST['name'];
-		$sobrenome =  $_POST['lastname'];
+		$sobrenome = $_POST['lastname'];
 		$email = $_POST['email']; 
 		$numero = $_POST['number'];
 		$senha = $_POST['password'];
-		$confirmarsenha = $_POST['confirmpassword'];
+		$confirmar_senha = $_POST['password'];
 		$genero = $_POST['gender'];
 
-	$result = mysqli_query($conexao, "INSERT INTO usuarios (nome, sobrenome, email, numero, senha, confirmarsenha, genero) VALUES ('$nome', '$sobrenome', '$email', '$numero', '$senha', '$confirmarsenha', '$genero')");
+	$result = mysqli_query($conexao, "INSERT INTO usuarios (nome, sobrenome, email, numero, senha, confirmarsenha, genero) VALUES ('$nome', '$sobrenome', '$email', '$numero', '$senha', '$confirmar_senha', '$genero')");
 }
 
 ?>
