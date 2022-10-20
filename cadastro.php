58f1eca7b5f88fd5a6213ecf28ff22959a53b876
AnaJuliaTavares <109613769+AnaJuliaTavares@users.noreply.github.com>
2022-10-20 14:55:15 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -8,62 +8,50 @@
     <link rel="stylesheet" href="cadastro.css">
     <title>Cadastro Miadote</title>
 </head>
-
 <body>
     <div class="container">
-
         <div class="form-image">
             <img src="undraw_good_doggy_re_eet7.svg">
         </div>
-
         <div class="form">
             <form action="validacao.php" method="post">
-
                 <div class="logo">
                     <img src="logogato.png" width="160" height="65" />
                     <div class="login-button">
                         <button> <a href="login.php"> Entrar </a></button>
                     </div>
                 </div>
-
                 <div class="form-header">
                     <div class="title">
                         <h1>Criar conta </h1>
                     </div>
                 </div>
-
                 <div class="input-group">
                     <div class="input-box">
                         <label for="name">Nome</label>
                         <input id="name" type="text" name="name" placeholder="Digite seu nome" required>
                     </div>
-
                     <div class="input-box">
                         <label for="lastname">Sobrenome</label>
                         <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required>
                     </div>
-
                     <div class="input-box">
                         <label for="email">Email</label>
                         <input id="email" type="email" name="email" placeholder="Digite seu email" required>
                     </div>
-
                     <div class="input-box">
                         <label for="number">Celular</label>
                         <input id="number" type="tel" name="number" placeholder="(xx) xxxx-xxxx" required>
                     </div>
-
                     <div class="input-box">
                         <label for="password">Senha</label>
                         <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
                     </div>
-
                     <div class="input-box">
                         <label for="Confirmpassword">Confirme sua senha</label>
                         <input id="Confirmpassword" type="password" name="Confirmpassword" placeholder="Digite sua senha" required>
                     </div>
                 </div>
-
                 <div class="gender-inputs">
                     <div class="gender-title">
                         <h6> Gênero</h6>
@@ -79,5 +67,4 @@
         </div>
     </div>
 </body>
-
 </html>
