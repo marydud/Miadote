79eca057c2370bae62cdf9b4c1aa56317cb91ffc
AnaJuliaTavares <109613769+AnaJuliaTavares@users.noreply.github.com>
2022-10-24 12:46:45 -0400

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -28,37 +28,24 @@
                 </div>
                 <div class="input-group">
                     <div class="input-box">
-                        <label for="name">Nome</label>
+                        <label for="name">Nome completo</label>
                         <input id="name" type="text" name="name" placeholder="Digite seu nome" required>
                     </div>
-                    <div class="input-box">
-                        <label for="lastname">Sobrenome</label>
-                        <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required>
-                    </div>
+                
                     <div class="input-box">
                         <label for="email">Email</label>
                         <input id="email" type="email" name="email" placeholder="Digite seu email" required>
                     </div>
                     <div class="input-box">
-                        <label for="number">Celular</label>
+                        <label for="number">Número para contato</label>
                         <input id="number" type="tel" name="number" placeholder="(xx) xxxx-xxxx" required>
                     </div>
                     <div class="input-box">
                         <label for="password">Senha</label>
                         <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
                     </div>
-                    <div class="input-box">
-                        <label for="Confirmpassword">Confirme sua senha</label>
-                        <input id="Confirmpassword" type="password" name="Confirmpassword" placeholder="Digite sua senha" required>
-                    </div>
                 </div>
-                <div class="gender-inputs">
-                    <div class="gender-title">
-                        <h6> Gênero</h6>
-                    </div>
-                    <div class="input-box">
-                        <input id="gender" type="text" name="gender" placeholder="Digite seu gênero" required>
-                    </div>
+    
                     <div class="button">
                         <button type="submit" name="submit">Cadastrar</button>
                     </div>
