66716dd5954a062d1b3ec890df9c026f6c96de1a
AnaJuliaTavares <109613769+AnaJuliaTavares@users.noreply.github.com>
2022-10-24 22:52:56 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>Miadote</title>
+
+</head>
+<body>
+    <br><br><br>
+    <h1> MIADOTE </h1>
+<h2> hehe<h2> <br><br><br><br><br>
+        <div class="container">
+        <div class="box">
+            <a href="login.php">login</a>
+        </div><br>
+        <div class="box">
+            <a href="cadastro.php">Cadastre-se</a>
+        </div>
+</div>
+</body>
+</html>
